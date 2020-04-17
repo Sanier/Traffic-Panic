bd9d6475a642959a40384b9ae598b3ab8cc0aa24
Alexandr <sanier20@mail.ru>
2020-04-17 12:12:13 +0300

    First save project

---
+++ Assets/Scripts/Traffic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using static System.Math;
+
+public class Traffic : MonoBehaviour
+{
+
+    [SerializeField] float CarSpeed = 2f;
+
+    public TrafficLight trafficLight;
+
+    private NavMeshAgent navMeshAgent;
+    
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void FixedUpdate()
+    {
+        CarMovement();
+    }
+
+    public void CarMovement()
+    {
+
+        //if (trafficLight.flag == true)
+        //{
+            navMeshAgent.Move(Vector3.forward * CarSpeed); //car.MovePosition(transform.position + transform.forward * CarSpeed);    //smooth movement of the car
+        //}
+
+        //else
+        //    navMeshAgent.isStopped = true; //car.drag = 2000;
+    }
+}
---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrafficLight : MonoBehaviour
+{
+    public Light redlight;
+    public Light greenlight;
+
+    public bool flag = false;
+
+    private Touch touch;
+
+    public float maxDis = 1f;
+    
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        greenlight.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TrafLight();
+        print(flag);
+
+    }
+
+    public void TrafLight() //traffic light
+    {
+        if (greenlight.enabled == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) //touch.phase == TouchPhase.Began
+            {
+                flag = false;
+                greenlight.enabled = false;
+                redlight.enabled = true;
+                return;
+            }
+        }
+
+        else if (redlight.enabled == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) //Input.GetKeyDown(KeyCode.Space)
+            {
+                flag = true;
+                greenlight.enabled = true;
+                redlight.enabled = false;
+                return;
+            }
+        }
+    }
+}
